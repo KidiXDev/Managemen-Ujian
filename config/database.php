03b2276b2083f7686e8d979c7fa2d97073580d45
KidiXDev <dikhiardian55@gmail.com>
2025-03-16 20:49:04 +0700

    Add configuration for real_data database connection

--- config/database.php
+++ config/database.php
@@ -63,6 +63,25 @@ return [
             ]) : [],
         ],
 
+        'real_data' => [
+            'driver' => 'mysql',
+            'host' => env('DB_DATA_HOST', '127.0.0.1'),
+            'port' => env('DB_DATA_PORT', '3306'),
+            'database' => env('DB_DATA_DATABASE', 'forge'),
+            'username' => env('DB_DATA_USERNAME', 'forge'),
+            'password' => env('DB_DATA_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
@@ -125,7 +144,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
